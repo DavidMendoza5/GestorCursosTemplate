9b758a3b922bafd2b162fe59becb23b9506378cb
DavidMendoza5 <cruz-azul99@hotmail.com>
2020-05-05 18:35:25 -0500

    Se agregó un footer

--- src/App.vue
+++ src/App.vue
@@ -12,8 +12,51 @@
     <v-content>
       <router-view/>
     </v-content>
+  <v-footer
+    dark
+    padless
+  >
+    <v-card
+      class="flex"
+      flat
+      tile
+    >
+      <v-card-title class="teal">
+        <strong class="subheading">¡Contáctanos!</strong>
+
+        <v-spacer></v-spacer>
+
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-4"
+          dark
+          icon
+          href="https://www.facebook.com/angeljesus.canpech"
+        >
+          <v-icon size="24px">{{ icon }}</v-icon>
+        </v-btn>
+      </v-card-title>
+
+      <v-card-text class="py-2 white--text text-center">
+        {{ new Date().getFullYear() }} — <strong>HuasTechnologies</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
+
   </v-app>
 </template>
+
+
+<script>
+  export default {
+    data: () => ({
+      icons: [
+        'mdi-facebook'
+      ],
+    }),
+  }
+</script>
 
 <!--
 <script>
