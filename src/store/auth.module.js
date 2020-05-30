d8c17e2bf2d20e50f693bb5430a1aca934ad8a0d
DavidMendoza5 <cruz-azul99@hotmail.com>
2020-05-30 18:32:13 -0500

    Se agregó el auth, el cual se usa para saber si se autenticó correctamente al usuario

---
+++ src/store/auth.module.js
@@ -0,0 +1,44 @@
+import AuthService from '../services/auth.service';
+
+const user = JSON.parse(localStorage.getItem('user'));
+const initialState = user
+    ? { status: { loggedIn: true }, user }
+    : { status: { loggedIn: false }, user: null };
+
+  export const auth = {
+    namespaced: true,
+    state: initialState,
+    actions: {
+        login({ commit }, user) {
+            return AuthService.login(user).then(
+                user => {
+                    commit('login success', user);
+                    return Promise.resolve(user);
+                },
+                error => {
+                    commit('login failure');
+                    return Promise.reject(error); 
+                }
+            );
+        },
+        logout({ commit }) {
+            AuthService.logut();
+            commit('logout');
+        }
+        //Faltan los registros
+    },
+    mutations: {
+        loginSuccess(state, user) {
+            state.status.loggedIn = true;
+            state.user = user;
+        },
+        loginFailure(state) {
+            state.status.loggedIn = false;
+            state.user = null;
+        },
+        logout(state) {
+            state.status.loggedIn = false;
+            state.user = null;
+        }
+    }
+  }
