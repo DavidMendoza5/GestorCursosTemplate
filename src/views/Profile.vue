57932e93c5b13c961f779f17bee56f062c908ae2
DavidMendoza5 <cruz-azul99@hotmail.com>
2020-05-30 18:30:41 -0500

    Se agregó la vista Profile, la cual se muestra al iniciar sesión

---
+++ src/views/Profile.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-card>
+        <v-card-title>Email: {currentUser.correo}</v-card-title>      
+    </v-card>
+</template>
+
+<script>
+export default {
+    name: 'Profile',
+    computed: {
+        currentUser() {
+            return this.$store.state.auth.user;
+        }
+    },
+    mounted() {
+        if(!this.currentUser) {
+            this.$router.push('/LogIn');
+        }
+    }
+}
+</script>
