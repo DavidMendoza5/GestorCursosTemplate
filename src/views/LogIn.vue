a46f2712bad00ddb2fbfa0765fe780e7f2e24fde
DavidMendoza5 <cruz-azul99@hotmail.com>
2020-05-30 15:09:32 -0500

    Se modificó la vista del Login

--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -1,17 +1,35 @@
 <template>
-  <v-parallax
-    dark
-    src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
-    height="1080" width="1920"
-  >
-    <v-row
-      align="center"
-      justify="center"
-    >
-      <v-col class="text-center" cols="12">
-        <h1 class="display-1 font-weight-thin mb-4">Logn In</h1>
-        <h4 class="subheading">Inicia sesión</h4>
-      </v-col>
-    </v-row>
-  </v-parallax>
+  <v-app>
+    <v-card width="400" class="mx-auto mt-5">
+      <v-card-title>
+        <h1 class="display-1">Login</h1>
+      </v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-text-field 
+          prepend-icon="mdi-account-circle"
+          label="Username"/>
+          <v-text-field 
+          prepend-icon="mdi-lock"
+          :type="showPassword ? 'text' : 'password'"
+          label="Password"
+          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+          @click:append="showPassword = !showPassword"/>
+        </v-form>
+      </v-card-text>
+      <v-divider></v-divider>
+      <v-card-action>
+        <v-btn color="info" class="mx-2" to="/SignUp">Register</v-btn>
+        <v-btn color="success">Login</v-btn>
+      </v-card-action>
+    </v-card>
+  </v-app>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    showPassword: false
+  }),
+}
+</script>
