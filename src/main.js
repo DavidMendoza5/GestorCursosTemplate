83b6c0f70d151cff2092bd416802755366dddabb
DavidMendoza5 <cruz-azul99@hotmail.com>
2020-05-30 19:47:56 -0500

    Se agregó el vuex y vee-validate

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import Vuex from 'vuex';
+import veeValidate from 'vee-validate';
 import '@babel/polyfill'
 
+Vue.use(Vuex);
+Vue.use(veeValidate);
 Vue.config.productionTip = false
 
 new Vue({
