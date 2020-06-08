d889e71eb7987c7844236cb9476f7bd5b18d477d
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2020-06-08 15:49:19 -0500

    Se añadió una ruta del perfil para el estudiante

--- src/router/index.js
+++ src/router/index.js
@@ -3,46 +3,58 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: () => import('../views/Home.vue')
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: () => import('../views/About.vue')
-  },
-  {
-    path: '/LogIn',
-    name: 'LogIn',
-    component: () => import('../views/LogIn.vue')
-  },
-  {
-    path: '/SignUp',
-    name: 'SignUp',
-    component: () => import('../views/SignUp.vue')
-  },
-  {
-    path: '/VerCursos',
-    name: 'VerCursos',
-    component: () => import('../views/VerCursos.vue')
-  },
-  {
-    path: '/VerDocentes',
-    name: 'VerDocentes',
-    component: () => import('../views/VerDocentes.vue')
-  },
-  {
-    path: '/Profile',
-    name: 'Profile',
-    component: () => import('../views/Profile.vue')
-  }
+const routes = [{
+        path: '/',
+        name: 'Home',
+        component: () =>
+            import ('../views/Home.vue')
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: () =>
+            import ('../views/About.vue')
+    },
+    {
+        path: '/LogIn',
+        name: 'LogIn',
+        component: () =>
+            import ('../views/LogIn.vue')
+    },
+    {
+        path: '/SignUp',
+        name: 'SignUp',
+        component: () =>
+            import ('../views/SignUp.vue')
+    },
+    {
+        path: '/VerCursos',
+        name: 'VerCursos',
+        component: () =>
+            import ('../views/VerCursos.vue')
+    },
+    {
+        path: '/VerDocentes',
+        name: 'VerDocentes',
+        component: () =>
+            import ('../views/VerDocentes.vue')
+    },
+    {
+        path: '/Profile',
+        name: 'Profile',
+        component: () =>
+            import ('../views/Profile.vue')
+    },
+    {
+        path: '/ProfileEst',
+        name: 'ProfileEstudiante',
+        component: () =>
+            import ('../views/profileEstudiante.vue')
+    }
 ]
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
 export default router
