ff093859fdaec749db56be161dd3881c969f6cdf
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2020-06-08 15:52:05 -0500

    Creación del servicio para el estudiante

---
+++ src/services/estudiante.service.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+const url = 'http://localhost:3800/api/';
+
+class EstudianteService {
+    postEstudiante(estudiante) {
+        console.log(estudiante);
+        return axios.post(url + 'crearEstudiante', {
+                nombre: estudiante.nombre,
+                correo: estudiante.correo,
+                password: estudiante.password,
+                cargo: estudiante.cargo,
+                telefono: estudiante.telefono,
+                conocimientos_previos: estudiante.conocimientos_previos,
+                _id: estudiante.id,
+                curso: estudiante.curso
+            }) //post Estudiante
+    }
+}
+
+export default new EstudianteService();
