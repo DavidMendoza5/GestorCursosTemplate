9b38505be690ea216937786ae22846607777d8f0
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2020-06-11 17:37:12 -0500

    Se creó el método crear curso

--- src/services/cursos.service.js
+++ src/services/cursos.service.js
@@ -3,8 +3,28 @@ const url = 'http://localhost:3800/api/';
 
 class CursoService {
     getCursos() {
-        return axios.get(url+'obtenerCursoDisponible')
+        return axios.get(url + 'obtenerCursoDisponible')
     }
-} 
+
+    crearCurso(curso) {
+        console.log(localStorage.getItem('token'));
+        var config = {
+            headers: { 'Authorization': localStorage.getItem('token') }
+        };
+        return axios.post(url + 'crearCurso', {
+            nombre: curso.nombre,
+            docente: curso.docente,
+            fecha_inicio: curso.fecha_inicio,
+            fecha_final: curso.fecha_final,
+            status: curso.status,
+            hora: curso.hora,
+            duracion: curso.duracion,
+            precio: curso.precio,
+            cupoLimite: curso.cupoLimite,
+            descripcion: curso.descripcion,
+            requisitos: curso.requisitos
+        }, config)
+    }
+}
 
 export default new CursoService();
