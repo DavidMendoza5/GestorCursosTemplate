afd131cfaa43e69d4a07c97cd03d5074969909e0
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2020-06-16 12:14:52 -0500

    Se añadió el botón de modificar datos del estudiante

--- src/views/profileEstudiante.vue
+++ src/views/profileEstudiante.vue
@@ -3,6 +3,9 @@
         <h1>Hola {{ currentUserEst.nombre }}</h1>
         <h2>Correo: {{currentUserEst.correo}}</h2>
         <h2>Id: {{currentUserEst.id}}</h2>
+        <v-card>
+            <v-btn class="ma-3" color="success" to="/actualizarEst">Modificar datos</v-btn>
+        </v-card>
     </div>
 </template>
 <script>
