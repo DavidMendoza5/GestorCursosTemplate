5a83732b9c6448df2072b5402da792a9aebc3fb9
angelcan58 <angelcan58@gmail.com>
2020-06-16 12:11:39 -0500

    se importo vue2-google-maps

--- src/main.js
+++ src/main.js
@@ -6,6 +6,14 @@ import vuetify from './plugins/vuetify';
 import Vuex from 'vuex';
 import veeValidate from 'vee-validate';
 import '@babel/polyfill'
+import * as VueGoogleMaps from "vue2-google-maps";
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: "AIzaSyBiVhuBuYmsQ7jQ9gaHwDeG5p5aXeUVedo",
+    libraries: "places" // necessary for places input
+  }
+});
 
 Vue.use(Vuex);
 Vue.use(veeValidate);
