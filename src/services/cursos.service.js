e6ae2e600671d247e86a67209483f82744ffc01f
angelcan58 <angelcan58@gmail.com>
2020-06-15 19:58:33 -0500

    se agrego Search

--- src/services/cursos.service.js
+++ src/services/cursos.service.js
@@ -5,13 +5,34 @@ class CursoService {
     getCursos() {
         return axios.get(url + 'obtenerCursoDisponible')
     }
-
+    getCursoById(id){
+        return axios.get(url + 'obtenerCurso/' + id)
+    }
     crearCurso(curso) {
         console.log(localStorage.getItem('token'));
         var config = {
             headers: { 'Authorization': localStorage.getItem('token') }
         };
         return axios.post(url + 'crearCurso', {
+            nombre: curso.nombre,
+            docente: curso.docente,
+            fecha_inicio: curso.fecha_inicio,
+            fecha_final: curso.fecha_final,
+            status: curso.status,
+            hora: curso.hora,
+            duracion: curso.duracion,
+            precio: curso.precio,
+            cupoLimite: curso.cupoLimite,
+            descripcion: curso.descripcion,
+            requisitos: curso.requisitos
+        }, config)
+    }
+
+    ActualizarCurso(Curso) {
+        var config = {
+            headers: { 'Authorization': localStorage.getItem('token') }
+        };
+        return axios.put(url + '/actualizarCurso', {
             nombre: curso.nombre,
             docente: curso.docente,
             fecha_inicio: curso.fecha_inicio,
