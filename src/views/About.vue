f43a9922f2e3829550a139237907935b932968f5
angelcan58 <angelcan58@gmail.com>
2020-06-16 12:13:37 -0500

    se modifico la ruta About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-parallax
+    dark
+    src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+    height="1080" width="1920"
+  >
+  <v-card width="400" class="mx-auto mt-5">
+        <v-img
+          height="300"
+          width="400" 
+          src="../assets/logo.jpg">
+    </v-img>
+  </v-card>
+  <v-card width="800" class="mx-auto mt-5">
+    <h1>HuasTecnology</h1>
+    <h2>Mision</h2>
+    <p>La misión de HuasTechnology es: “Mejorar la educación actual, mediante el uso de la
+tecnología, dándole un apoyo al maestro y una mejor enseñanza al alumno”.
+La empresa iniciará con la creación de un software educativo, el cual será de apoyo para los
+docentes, mejorando la gestion de los cursos impartidos.
+Lo que pretende nuestra empresa es mejorar la gestion de los cursos impartidos, desde tener un chat para que alumnos
+y docentes puedan mantenerse comunicados, hasta que el alumno conozca toda la gama de cursos que se imparten.</p>
+  <h2>Vision de la empresa</h2>
+  <p>Crear software de calidad para docentes y alumnos para mejorar la gestion de cursos</p>
+  </v-card>
+  </v-parallax>
 </template>
