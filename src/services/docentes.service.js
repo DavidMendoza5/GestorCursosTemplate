2ad24f82106ce5943bfbfd08f326ccea38edb427
DavidMendoza5 <cruz-azul99@hotmail.com>
2020-06-17 20:02:20 -0500

    Se agregó la función para obtener a un docente por su ID

--- src/services/docentes.service.js
+++ src/services/docentes.service.js
@@ -5,6 +5,9 @@ class DocenteService {
     getDocentes() {
         return axios.get(url+'obtenerDocentes')
     }
+    getDocentesById(id) {
+        return axios.get(url + 'obtenerDocente/' + id)
+    }
 } 
 
 export default new DocenteService();
