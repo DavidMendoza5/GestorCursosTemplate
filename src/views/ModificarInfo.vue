1bf2fc4a8435817960df6c11392279036208812c
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2020-06-19 11:17:31 -0500

    Se agregó diseño en actualizar un docente

--- src/views/ModificarInfo.vue
+++ src/views/ModificarInfo.vue
@@ -1,68 +1,71 @@
 <template>
-    <v-form 
-        ref="form"
-        v-model="valid"
-        @submit.prevent="modificarDatos(currentUser.id)">
-    <h2>Hola {{ currentUser.nombre }}, ingrese los datos que desea modificar</h2>
-    <v-container>
-    <v-row>
-        <v-col
-          cols="12"
-          md="4"
-        >
-          <v-text-field
+  <v-app>
+    <v-banner color="teal">
+      <v-row align="center" justify="center">
+        <v-col class="text-center">
+          <h1 class="font-weight-light mx-auto">¡Bienvenid@! {{ currentUser.nombre }}, ingrese los datos que desea modificar</h1>
+        </v-col>
+      </v-row>
+    </v-banner>
+
+    <v-form ref="form" v-model="valid" @submit.prevent="modificarDatos(currentUser.id)">   
+      <v-container>
+        <v-card max-width="544" class="mx-auto ma-10">
+          <v-toolbar flat color="grey darken-4">
+            <v-toolbar-title class="font-weight-light white--text ml-6">Datos a actualizar</v-toolbar-title>
+            <v-spacer></v-spacer>
+          </v-toolbar>
+          <v-card-text>
+            <v-text-field
             v-model="docente.nombre"
             :rules="nameRules"
             :counter="20"
             label="Nombre"
-            required
-          ></v-text-field>
-        </v-col>
-        <v-col
-          cols="12"
-          md="4"
-        >
-          <v-text-field
+            required>
+            </v-text-field>
+            <v-text-field
             v-model="docente.correo"
             :rules="emailRules"
             label="E-mail"
-            required
-          ></v-text-field>
-        </v-col>
-        <v-col
-          cols="12"
-          md="4"
-        >
-          <v-text-field
+            required>
+            </v-text-field>
+            <v-text-field
             v-model="docente.cargo"
             :rules="nameRules"
             label="Cargo"
-            required
-          ></v-text-field>
-        </v-col>
-      </v-row>
-    <div class="ma-3">
-      <v-btn
-      :disabled="!valid"
-      color="success"
-      class="mr-4"
-      type="submit"
-      >
-        Modificar
-      </v-btn>
-    </div>
-    <div class="ma-3">
-        <h2>Sus datos actuales</h2>
-        <h3>Nombre: {{ currentUser.nombre }}</h3>
-        <h3>Correo: {{ currentUser.correo }}</h3>
-        <h3>Cargo: {{ currentUser.cargo }}</h3>
-    </div>
-    </v-container>
-        <v-snackbar v-model="snackbar">
-      <!--{{ message.message }}--> Docente modificado correctamente
-      <v-btn color="blue" text @click="snackbar=false">cerrar</v-btn>
-    </v-snackbar>
-  </v-form>
+            required>
+            </v-text-field>
+          </v-card-text>
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-btn :disabled="!valid" color="grey darken-4" class="mr-4 white--text" type="submit">
+              Modificar
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+
+        <v-card max-width="344" class="mx-auto ma-10">
+          <v-toolbar flat color="grey darken-4">
+            <v-toolbar-title class="font-weight-light white--text ml-6">Sus datos actuales</v-toolbar-title>
+            <v-spacer></v-spacer>
+          </v-toolbar>
+          <v-card-text>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title class="font-weight-light mb-2">ID {{currentUser.id}}</v-list-item-title>
+                  <v-list-item-title class="font-weight-light mb-2">Nombre {{currentUser.nombre}}</v-list-item-title>
+                  <v-list-item-title class="font-weight-light mb-2">Email {{currentUser.correo}}</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+          </v-card-text>
+        </v-card>
+      </v-container>
+      <v-snackbar v-model="snackbar">
+        Docente modificado correctamente
+        <v-btn color="blue" text @click="snackbar=false">cerrar</v-btn>
+      </v-snackbar>
+    </v-form>
+  </v-app>
 </template>
 
 <script>
